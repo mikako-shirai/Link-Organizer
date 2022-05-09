acfb8dcffaa3186e3f43627413eefd010eb88ab6
mikako-shirai <mkkshirai@gmail.com>
2022-05-09 16:04:56 +0900

    feat: add new schema for folders

--- server/models/folders.js
+++ server/models/folders.js
@@ -1,2 +1,15 @@
 import mongoose from "mongoose";
 
+const foldersSchema = mongoose.Schema({
+  folderName: String,
+  links: [String],
+  dateCreated: {
+    type: Date,
+    default: new Date()
+  },
+  tags: [String]
+});
+
+const Folders = mongoose.model("Folders", foldersSchema);
+
+export default Folders;
