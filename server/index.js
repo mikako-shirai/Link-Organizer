9df372a2e4f01c1f9a6cbb4bfacbf54f4d21c31a
mikako-shirai <mkkshirai@gmail.com>
2022-05-10 12:56:29 +0900

    chore: update files

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ import mongoose from "mongoose";
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import path from "path";
 
 import folderRoutes from "./routes/folders.js";
 import linkRoutes from "./routes/links.js";
@@ -15,6 +16,13 @@ app.use("/folders", folderRoutes);
 app.use("/links", linkRoutes);
 
 const PORT = process.env.PORT || 5000;
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => app.listen(PORT, () => console.log(`Server running on : http://localhost:${PORT}`)))
