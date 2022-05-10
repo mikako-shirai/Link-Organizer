2a0f7fe524589b780ec5f7cfcd1511ff9dc734d2
mikako-shirai <mkkshirai@gmail.com>
2022-05-10 18:32:06 +0900

    chore: update files

--- client/src/components/all/links.jsx
+++ client/src/components/all/links.jsx
@@ -8,7 +8,10 @@ const Links = ({ links }) => {
         return (
           <>
             <div key={index} className="grid-link">
-              <div>{link}</div>
+              {/* <div>{link}</div> */}
+              <object data="https://duckduckgo.com/?q=js+Refused+to+frame+%27%3CURL%3E%27+because+an+ancestor+violates+the+following+Content+Security+Policy+directive%3A+%22frame-ancestors+%27none%27%22.&t=brave&ia=web">
+                <embed src="https://duckduckgo.com/?q=js+Refused+to+frame+%27%3CURL%3E%27+because+an+ancestor+violates+the+following+Content+Security+Policy+directive%3A+%22frame-ancestors+%27none%27%22.&t=brave&ia=web" />
+              </object>
             </div>
             <div key={index} className="grid-link">
               <div>{link}</div>
