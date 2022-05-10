506d041e90aa27282735d297a4f9b50f76b32a28
mikako-shirai <mkkshirai@gmail.com>
2022-05-10 15:42:24 +0900

    feat: add elements to Folders component

--- client/src/components/all/Folders.jsx
+++ client/src/components/all/Folders.jsx
@@ -1,9 +1,28 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 const Folders = () => {
+  const [folders, setFolders] = useState([]);
+
+  const getAllFolders = async () => {
+    const res = await axios.get("/folders");
+    const allFolders = res.data.data;
+    setFolders(allFolders);
+  };
+
+  useEffect(() => {
+    getAllFolders();
+  }, []);
+
   return (
     <div className="folders">
-      <div>Folders</div>
+      <div className="folders-grid">
+        {folders.map((folder, index) => {
+          return (
+            <div key={index} className="grid-folder">{folder.folderName}</div>
+          );
+        })}
+      </div>
     </div>
   );
 };
